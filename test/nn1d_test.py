418ee649ad91697592c1f45106a3eb94187fa89a
ev-i <47784335+ev-i@users.noreply.github.com>
2022-10-04 12:14:38 +0300

    some test added

---
+++ test/nn1d_test.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("digital-image-processing")
+from src.image_interpolation.nearest_neighbor_interpolation.one_d_interpolation import nn1d
+# from nn1d import get_nearest_x, fill_empty
+
+
+
+# Задан одномерный массив пикселей. Индекс элемента в массиве - координата пикселя. Значение элемента массива - яркость пикселя. Значение элемента None означает неизвестную яркость.
+pixels = [1, None, None, 20, None, None, 155]
+# Таким образом имеем три пикселя с известной яркостью - (x0), (x3), (x6) и четыре пикселя с неизвестной яркостью (x1), (x2), (x4), (x5)
+print(pixels)
+# Необходимо задать яркость пикселям с неизвестной яркостью интерполяцией по ближайшему соседу
+print(nn1d.fill_empty(pixels))
